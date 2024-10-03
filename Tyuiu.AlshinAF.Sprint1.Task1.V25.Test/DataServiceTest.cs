782a4ee75cdaae3fc0ebf2f3a97aff95bb255347
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-03 20:26:52 +0500

    Task1

---
+++ Tyuiu.AlshinAF.Sprint1.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.AlshinAF.Sprint1.Task1.V25.Lib;
+
+namespace Tyuiu.AlshinAF.Sprint1.Task1.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1, res);
+        }
+    }
+}
--- Tyuiu.AlshinAF.Sprint1.Task1.V25/Program.cs
+++ Tyuiu.AlshinAF.Sprint1.Task1.V25/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.AlshinAF.Sprint1.Task0.V5.Lib;
-namespace Tyuiu.AlshinAF.Sprint1.Task0.V5
+﻿using Tyuiu.AlshinAF.Sprint1.Task1.V25.Lib;
+namespace Tyuiu.AlshinAF.Sprint1.Task1.V25
 {
     class Program
     {
@@ -10,21 +10,27 @@ namespace Tyuiu.AlshinAF.Sprint1.Task0.V5
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема:  Базовые навыки работы в C#                                       *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Тема:  Организация ввода/вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
             Console.WriteLine("* Выполнил: Альшин Артур Фаритович | РППб-24-1                            *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет выражение (1+2)*(1+9/3)           *");
-            Console.WriteLine("* и печатает результат на экране                                          *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x*y)/(1+x) и печатает результат на экране*");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* (1+2)*(1+9/3)                                                           *");
+
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine(ds.Calculate());
+            Console.WriteLine(ds.Calculate(x, y));
 
             Console.ReadLine();
         }
