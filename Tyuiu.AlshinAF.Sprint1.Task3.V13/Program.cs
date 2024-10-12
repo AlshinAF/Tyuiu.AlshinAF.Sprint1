91f1fc0ef90ab1f84449b4864988ae862d020278
Tyuiu.AlshinAF@outlook.com <alshin06@gmail.com>
2024-10-13 03:33:15 +0500

    Task3

---
+++ Tyuiu.AlshinAF.Sprint1.Task3.V13/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.AlshinAF.Sprint1.Task3.V13.Lib;
+namespace Tyuiu.AlshinAF.Sprint1.Task1.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Альшин А. Ф. | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема:  Организация ввода/вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Альшин Артур Фаритович | РППб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая позволяет найти произведение цифр,          *");
+            Console.WriteLine("* заданного трехзначного числа                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x = 634;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.MultiplyOfDigits(x));
+
+            Console.ReadLine();
+        }
+    }
+}
